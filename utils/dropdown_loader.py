886ebc4b931d0e7310d2c1ae56c708d8a3d27072
soem-cpu <soe.m@reachforequity.org>
2026-03-11 01:13:30 +0700

    Update dropdown_loader.py

--- utils/dropdown_loader.py
+++ utils/dropdown_loader.py
@@ -9,14 +9,12 @@ def load_dropdowns(df):
             d.setdefault(var, set()).add(val)
     return d
 
-
 def load_service_pairs(df):
     return set(
         (normalize(r.get("Township")), normalize(r.get("Base Code")))
         for _, r in df.iterrows()
         if normalize(r.get("Township")) and normalize(r.get("Base Code"))
     )
-
 
 def load_basecode_level(df):
     return {
